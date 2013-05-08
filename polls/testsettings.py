05ec859a9134a8d9deacd999a2f9691eaff9c637
Daniele Procida <daniele@vurt.org>
2013-05-08 21:23:54 +0100

    not sure...

---
+++ polls/testsettings.py
@@ -0,0 +1,13 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'pollstests.db',
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '', 
+        'PORT': '',
+    }
+}
+
+SECRET_KEY = 'i4mth3m0sts3cr3t0f4lls3tti1ngs'
